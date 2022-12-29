e4f91acd3fa06b1da73ff010280125fb3e53d7c6
Bogdan <bohdan@forecasa.com>
2022-12-29 19:58:03 +0200

    add buttons to templates

--- todolistproject/todolist/views.py
+++ todolistproject/todolist/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User, auth
 from django.contrib import messages
-# Create your views here.
 
+
+# Create your views here.
 def index(request):
-    return render(request,'index.html')
+    return render(request, 'index.html')
+
 
 def register(request):
     if request.method == 'POST':
@@ -21,13 +23,14 @@ def register(request):
                 messages.info(request, 'Username already used')
                 return redirect('register')
             else:
-                User.objects.create_user(username=username,email=email,password=password)
-                user.save()
+                User.objects.create_user(username=username, email=email, password=password)
+                User.save()
                 return redirect('login')
         else:
             messages.info(request, 'Passwords do not match')
             return render(request, 'register.html')
     return render(request, 'register.html')
+
 
 def login(request):
     if request.method == 'POST':
